c3cf1795fcacb7b6359fc860b46a5a0059afc077
Antony Mwendwa <antonymwendwa408@gmail.com>
2026-01-30 16:12:03 +0300

    label

--- roll.js
+++ roll.js
@@ -1,3 +1,8 @@
 let x;
 let y;
 let z;
+document.getElementById("rollBtn").addEventListener("click", function() {
+    x = Math.floor(Math.random() * 6) + 1;
+    y = Math.floor(Math.random() * 6) + 1;
+    z = Math.floor(Math.random() * 6) + 1;
+}
